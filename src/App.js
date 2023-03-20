ed9667ff411cc15f68a468711c990e427c213ae7
Emre Şahin <190254064@ogr.alanya.edu.tr>
2023-03-20 14:54:41 +0300

    Commerce JS API Fetch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-
 import "./App.scss";
+import { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Products, Navbar } from "./components";
-
-
+import { commerce } from "./lib/commerce";
 
 // function AppRoutes() {
 //   const routes = useRoutes([{ path: "/", element: <Home /> }]);
 // }
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     // <BrowserRouter>
     //   <AppRoutes />
     // </BrowserRouter>
     <>
-    <Navbar />
-    <Products />
+      <Navbar /> 
+      <Products />
     </>
   );
 }
